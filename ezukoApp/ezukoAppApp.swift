d50bcd827c4bf67457dca94efdc6287920542115
taikihamamoto <taiki200316@icloud.com>
2022-12-02 16:34:20 +0900

    Initial Commit

---
+++ ezukoApp/ezukoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ezukoAppApp.swift
+//  ezukoApp
+//
+//  Created by MacUser on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct ezukoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
